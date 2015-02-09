e83e71f8ba6fc08ad702fb2295b654eff193b43a
Guillaume Claret <dev@clarus.me>
2015-02-09 16:48:54 +0100

    Definition of the system calls

--- Computation.v
+++ Computation.v
@@ -1,15 +1,32 @@
 (** The definition of computations, used to represent interactive programs. *)
+Require Import Coq.NArith.NArith.
 Require Import ListString.All.
 
 Local Open Scope type.
 
 (** System calls. *)
 Module Command.
-  Inductive t :=.
+  Inductive t :=
+  | AskCard
+  | AskPIN
+  | CheckPIN (pin : N)
+  | AskAmount
+  | CheckAmount (amount : N)
+  | GiveAmount (amount : N)
+  | GiveCard
+  | ShowError (message : LString.t).
 
   (** The type of an answer for a command depends on the value of the command. *)
   Definition answer (command : t) : Type :=
     match command with
+    | AskCard => bool (* If the given card seems valid. *)
+    | AskPIN => option N (* A number or cancellation. *)
+    | CheckPIN _ => bool (* If the PIN number is valid. *)
+    | AskAmount => option N (* A number or cancellation. *)
+    | CheckAmount _ => bool (* If the amount can be withdrawn. *)
+    | GiveAmount _ => bool (* If the money was given. *)
+    | GiveCard => bool (* If the card was given. *)
+    | ShowError _ => unit (* Show an error message. *)
     end.
 End Command.
 
